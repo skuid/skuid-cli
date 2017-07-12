1ef7171a272ce120121da694efb978165fe15643
Zach McElrath <zach@skuidify.com>
2017-07-12 16:48:07 -0400

    consolidate setting of defaults

--- cmd/root.go
+++ cmd/root.go
@@ -39,37 +39,16 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	RootCmd.PersistentFlags().StringVar(&appClientID, "client-id", "", "OAuth Client ID")
-	RootCmd.PersistentFlags().StringVar(&appClientSecret, "client-secret", "", "OAuth Client Secret")
+	RootCmd.PersistentFlags().StringVar(&appClientID, "client-id", os.Getenv("SKUID_CLIENT_ID"), "OAuth Client ID")
+	RootCmd.PersistentFlags().StringVar(&appClientSecret, "client-secret", os.Getenv("SKUID_CLIENT_SECRET"), "OAuth Client Secret")
 	RootCmd.PersistentFlags().StringVar(&apiVersion, "api-version", "", "API Version")
-	RootCmd.PersistentFlags().StringVar(&host, "host", "",
+	RootCmd.PersistentFlags().StringVar(&host, "host", os.Getenv("SKUID_HOST"),
 		"API Host base URL, e.g. my-site.skuidsite.com for Skuid Platform or my-domain.my.salesforce.com for Salesforce")
 	RootCmd.PersistentFlags().StringVarP(&module, "module", "m", "", "Module name(s), separated by a comma.")
-	RootCmd.PersistentFlags().StringVarP(&password, "password", "p", "", "Skuid Platform / Salesforce Password")
+	RootCmd.PersistentFlags().StringVarP(&password, "password", "p", os.Getenv("SKUID_PW"), "Skuid Platform / Salesforce Password")
 	RootCmd.PersistentFlags().StringVarP(&targetDir, "dir", "d", "", "Input/output directory.")
-	RootCmd.PersistentFlags().StringVarP(&username, "username", "u", "", "Skuid Platform / Salesforce Username")
+	RootCmd.PersistentFlags().StringVarP(&username, "username", "u", os.Getenv("SKUID_UN"), "Skuid Platform / Salesforce Username")
 	RootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Display all possible logging info")
-
-	if username == "" {
-		username = os.Getenv("SKUID_UN")
-	}
-
-	if password == "" {
-		password = os.Getenv("SKUID_PW")
-	}
-
-	if host == "" {
-		host = os.Getenv("SKUID_HOST")
-	}
-
-	if appClientID == "" {
-		appClientID = os.Getenv("SKUID_CLIENT_ID")
-	}
-
-	if appClientSecret == "" {
-		appClientSecret = os.Getenv("SKUID_CLIENT_SECRET")
-	}
-
 }
 
 // initConfig reads in config file and ENV variables if set.
