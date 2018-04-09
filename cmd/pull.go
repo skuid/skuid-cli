9c992e21901ac24e83125a76a24185be82c8b4c1
Joshua Goodlett <joshuag@skuid.com>
2018-04-09 16:12:42 -0400

    Fixed typo

--- cmd/pull.go
+++ cmd/pull.go
@@ -49,7 +49,7 @@ var pullCmd = &cobra.Command{
 			requestedItems = []string{module}
 		} else if name != "" {
 			requestedItems = []string{name}
-			itemsKey = "name"
+			itemKey = "name"
 		}
 
 		query := url.Values{
