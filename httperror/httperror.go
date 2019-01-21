4d968a7965749fe8eb0a29bc740ea627c22e535f
Ben Hubbard <plusplusben@gmail.com>
2019-01-21 15:56:47 -0500

    fix error handling bug

--- httperror/httperror.go
+++ httperror/httperror.go
@@ -12,14 +12,18 @@ func New(status string, body io.Reader) error {
 	var httpError HTTPError
 	bodyBytes, err := ioutil.ReadAll(body)
 	if err != nil {
+		fmt.Println("Failed Reading")
 		return fmt.Errorf("%v - %v", err.Error(), "Could not parse error message")
 	}
+	bodyString := string(bodyBytes)
 	// Attempt to parse the body
 	err = json.Unmarshal(bodyBytes, &httpError)
 	if err != nil {
-		return fmt.Errorf("%v - %v", err.Error(), string(bodyBytes))
+		fmt.Println("Failed Parsing")
+		return fmt.Errorf("%v - %v", err.Error(), bodyString)
 	}
 
+	httpError.Body = bodyString
 	httpError.Status = status
 
 	return &httpError
@@ -36,7 +40,13 @@ type HTTPError struct {
 
 func (e *HTTPError) Error() string {
 	if e.ParsedError != "" && e.ParsedMessage != "" {
-		return fmt.Sprintf("%v - %v - %v", e.StatusCode, e.ParsedError, e.ParsedMessage)
+		return fmt.Sprintf("%v - %v - %v", e.Status, e.ParsedError, e.ParsedMessage)
 	}
-	return fmt.Sprintf("%v - %v", e.StatusCode, e.Body)
+	if e.ParsedError != "" {
+		return fmt.Sprintf("%v - %v", e.Status, e.ParsedError)
+	}
+	if e.ParsedMessage != "" {
+		return fmt.Sprintf("%v - %v", e.Status, e.ParsedMessage)
+	}
+	return fmt.Sprintf("%v - %v", e.Status, e.Body)
 }
