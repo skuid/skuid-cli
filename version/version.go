0cc8181ab9be814731c6d0ed4f3eab31d905f000
Adam Cofer <acofer@users.noreply.github.com>
2021-01-07 17:20:57 -0500

    PLIN-2705 cli should accept new app flag (#74)
    * PLIN-2705 Support new app name flag posted to pliny retrieve plan endpoint
    
    * Fix internal version number
    
    * Read version number from single place
    
    * Assign version at build time, not runtime

--- version/version.go
+++ version/version.go
@@ -1,18 +1,4 @@
 package version
 
-import (
-	"fmt"
-	"io/ioutil"
-)
-
 // Name of this version of Skuid CLI
 var Name = "unknown"
-
-func init() {
-	version, err := ioutil.ReadFile(".version")
-	if err != nil {
-		fmt.Printf("Could not read CLI version from .version: %s\n", err.Error())
-		return
-	}
-	Name = string(version)
-}
