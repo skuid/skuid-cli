14913b19abf90ebfc69b0d1a3c913c3fd176814e
Jared Hooper <jared.hooper@skuid.com>
2022-04-29 11:59:45 -0400

    tides: benchmarking done. fasthttp is my new bff

--- pkg/nlx/http_test.go
+++ pkg/nlx/http_test.go
@@ -10,8 +10,8 @@ import (
 	"github.com/valyala/fasthttp"
 
 	"github.com/skuid/tides/pkg"
-	"github.com/skuid/tides/pkg/constants"
 	"github.com/skuid/tides/pkg/logging"
+	"github.com/skuid/tides/pkg/util"
 )
 
 const (
@@ -22,12 +22,7 @@ func GetAccessToken(
 	host, username, password string, // optional
 ) (accessToken string, err error) {
 	logging.VerboseSeparator()
-	logging.VerboseLn("Starting NLX Login")
-
-	// if apiVersion == "" {
-	// 	logging.VerboseF("Setting API Version to DEFAULT_API_VERSION: %v\n", DEFAULT_API_VERSION)
-	// 	apiVersion = DEFAULT_API_VERSION
-	// }
+	logging.VerboseLn("Starting NLX Access Token Request")
 
 	// Need to acquire and release resources from fasthttp
 	// fasthttp handles multithreading for us and removes
@@ -64,13 +59,12 @@ func GetAccessToken(
 	// prep the request itself
 	req.Header.SetMethod(fasthttp.MethodPost)
 	req.Header.Add(fasthttp.HeaderContentType, "application/x-www-form-urlencoded")
-	req.Header.Add(fasthttp.HeaderUserAgent, fmt.Sprintf("Skuid-CLI/%s", constants.VERSION_NAME))
+	req.Header.Add(fasthttp.HeaderUserAgent, pkg.SkuidUserAgent)
 
 	// prep for the response
 	resp := fasthttp.AcquireResponse()
 	defer fasthttp.ReleaseResponse(resp)
 
-	// TODO: Log that we're attempting to login
 	logging.VerboseLn("Login Attempt...")
 
 	// perform the request. errors only pop up if there's an issue
@@ -91,7 +85,7 @@ func GetAccessToken(
 		return
 	}
 
-	logging.VerboseLn("Successfully logged in.")
+	logging.VerboseLn("Succeeded in Skuid NLX Access Token Retrieval.")
 
 	// anonymous struct for the response body unmarshalling from
 	// json. the field we want is "access_token" from the response
@@ -106,35 +100,87 @@ func GetAccessToken(
 
 	accessToken = responseJson.AccessToken
 
-	logging.VerboseLn(color.Green.Sprintf("Received Access Token: %v", accessToken))
+	logging.VerboseLn(color.Green.Sprintf("(Access Token: %v)", accessToken))
+
+	return
+}
+
+func GetAuthorizationToken(host, accessToken string) (authToken string, err error) {
+	logging.VerboseSeparator()
+	logging.VerboseLn("Starting NLX Access Token Request")
+
+	// Determine the URI
+	uri := fasthttp.AcquireURI()
+	defer fasthttp.ReleaseURI(uri)
+
+	// by passing `nil` as the first argument, the whole url will be parsed
+	// by the uri.parse function.
+	if err = uri.Parse(nil, []byte(host+"/api/v2/auth/token")); err != nil {
+		return
+	}
+
+	// prepare the http request
+	req := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(req)
+
+	// set uri
+	req.SetURI(uri)
+
+	// headers
+	req.Header.Add(fasthttp.HeaderAuthorization, fmt.Sprintf("Bearer %v", accessToken))
+	req.Header.Add(fasthttp.HeaderUserAgent, pkg.SkuidUserAgent)
+
+	// prep for the response
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(resp)
+
+	if err = fasthttp.Do(req, resp); err != nil {
+		return
+	}
+
+	// check the validity of the body and grab the access token
+	body := resp.Body()
+	if statusCode := resp.StatusCode(); statusCode != fasthttp.StatusOK {
+		err = fmt.Errorf("%s:\nStatus Code: %v\nBody: %v\n",
+			color.Red.Sprint("ERROR"),
+			color.Yellow.Sprint(statusCode),
+			color.Cyan.Sprint(string(body)),
+		)
+		return
+	}
+
+	// anonymous struct for the response body unmarshalling from
+	// json. the field we want is "token" from the response
+	// body
+	responseJson := struct {
+		AuthorizationToken string `json:"token"`
+	}{}
+
+	if err = json.Unmarshal(body, &responseJson); err != nil {
+		return
+	}
+
+	authToken = responseJson.AuthorizationToken
+
+	logging.VerboseLn(color.Green.Sprintf("(Auth Token: %v)", authToken))
 
 	return
 }
 
 func TestFasthttpMethods(t *testing.T) {
+	util.SkipIntegrationTest(t)
+	host := "https://jredhoop-subdomain.pliny.webserver:3000"
 	logging.SetVerbose(true)
-	if _, err := GetAccessToken("https://jredhoop-subdomain.pliny.webserver:3000", "jredhoop", "SkuidLocalDevelopment"); err != nil {
+
+	if at, err := GetAccessToken(
+		host, "jredhoop", "SkuidLocalDevelopment",
+	); err != nil {
 		color.Red.Println(err)
 		t.FailNow()
-	}
-}
-
-func BenchmarkFasthttpLogin(b *testing.B) {
-	logging.SetVerbose(false)
-	for i := 0; i < b.N; i++ {
-		if _, err := GetAccessToken("https://jredhoop-subdomain.pliny.webserver:3000", "jredhoop", "SkuidLocalDevelopment"); err != nil {
-			color.Red.Println(err)
-			b.FailNow()
-		}
-	}
-}
-
-func BenchmarkHttpLogin(b *testing.B) {
-	logging.SetVerbose(false)
-	for i := 0; i < b.N; i++ {
-		if _, err := pkg.SkuidNlxLogin2("https://jredhoop-subdomain.pliny.webserver:3000", "jredhoop", "SkuidLocalDevelopment", ""); err != nil {
-			color.Red.Println(err)
-			b.FailNow()
-		}
+	} else if _, err := GetAuthorizationToken(
+		host, at,
+	); err != nil {
+		color.Red.Println(err)
+		t.FailNow()
 	}
 }
---
+++ pkg/util/test.go
@@ -0,0 +1,15 @@
+package util
+
+import "testing"
+
+func SkipIntegrationTest(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+}
+
+func SkipBenchmark(b *testing.B) {
+	if testing.Short() {
+		b.Skip()
+	}
+}
