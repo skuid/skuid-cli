339f32875b438d2cf2ba3b6494436bde3dbc61e5
Jared Hooper <jared.hooper@skuid.com>
2022-07-07 20:51:42 -0400

    PLIN-3600: disable ui

--- cmd/tides.go
+++ cmd/tides.go
@@ -1,15 +1,12 @@
 package cmd
 
 import (
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
-	"github.com/skuid/tides/cmd/common"
 	"github.com/skuid/tides/pkg/constants"
 	"github.com/skuid/tides/pkg/flags"
 	"github.com/skuid/tides/pkg/logging"
-	"github.com/skuid/tides/ui"
 )
 
 var (
@@ -26,21 +23,21 @@ var (
 			DisableDefaultCmd: true,
 		},
 		Version: constants.VERSION_NAME,
-		PreRunE: common.LoggingValidation,
-		Run: func(cmd *cobra.Command, _ []string) {
+		// PreRunE: common.LoggingValidation,
+		// Run: func(cmd *cobra.Command, _ []string) {
 
-			// want to hide the logger if we're not file logging
-			if fileLogging, err := cmd.Flags().GetBool(flags.FileLogging.Name); err != nil {
-				logging.Get().Panicf("we need to know if we're file logging: %v", err)
-			} else if !fileLogging {
-				logging.DisableLogging()
-			}
+		// 	// want to hide the logger if we're not file logging
+		// 	if fileLogging, err := cmd.Flags().GetBool(flags.FileLogging.Name); err != nil {
+		// 		logging.Get().Panicf("we need to know if we're file logging: %v", err)
+		// 	} else if !fileLogging {
+		// 		logging.DisableLogging()
+		// 	}
 
-			p := tea.NewProgram(ui.Main(cmd))
-			if err := p.Start(); err != nil {
-				logging.Get().Errorf("Unable to Start User Interface: %v", err)
-			}
-		},
+		// 	p := tea.NewProgram(ui.Main(cmd))
+		// 	if err := p.Start(); err != nil {
+		// 		logging.Get().Errorf("Unable to Start User Interface: %v", err)
+		// 	}
+		// },
 	}
 )
 
