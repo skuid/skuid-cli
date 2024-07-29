ec9f4a382e6d68e7255609a0b7797544139a635c
techfg <barry@techfg.com>
2024-07-29 13:44:45 -0700

    chore: add tests for Value type
    Related to 92e22a4f1d0f8c886d877f5caa9b92832b793953

--- pkg/flags/types_test.go
+++ pkg/flags/types_test.go
@@ -2,6 +2,8 @@ package flags_test
 
 import (
 	"fmt"
+	"reflect"
+	"strconv"
 	"testing"
 
 	"github.com/skuid/skuid-cli/pkg/flags"
@@ -286,4 +288,119 @@ func TestGetCustomString(t *testing.T) {
 			assert.Equal(t, s, tc.wantValue)
 		})
 	}
+}
+
+type ValueTestSuite struct {
+	suite.Suite
+}
+
+func (suite *ValueTestSuite) TestNewValue() {
+	t := suite.T()
+	flagName := "myflag"
+	expectedDefaultValue := 100
+	multiplier := 500
+
+	fs := pflag.NewFlagSet("", pflag.ExitOnError)
+	parse := func(val string) (int, error) {
+		if iVal, err := strconv.Atoi(val); err != nil {
+			return 0, err
+		} else {
+			return iVal * multiplier, nil
+		}
+	}
+	p := new(int)
+	v := flags.NewValue(expectedDefaultValue, p, parse)
+	fs.VarP(v, flagName, "", "")
+
+	actualValue, err := fs.GetInt(flagName)
+	require.NoError(t, err)
+	assert.Equal(t, expectedDefaultValue, actualValue)
+	fs.Set(flagName, "2")
+	actualValue, err = fs.GetInt(flagName)
+	require.NoError(t, err)
+	assert.Equal(t, multiplier*2, actualValue)
+}
+
+func (suite *ValueTestSuite) TestNewValueWithRedact() {
+	t := suite.T()
+	flagName := "myflag"
+	expectedDefaultValue := "default"
+	expectedRedactedValue := "!!!!"
+	unredactedValue := "foobar"
+
+	fs := pflag.NewFlagSet("", pflag.ExitOnError)
+	parse := func(val string) (string, error) {
+		return fmt.Sprintf("%v::%v", val, val), nil
+	}
+	redact := func(string) string { return expectedRedactedValue }
+	p := new(string)
+	v := flags.NewValueWithRedact(expectedDefaultValue, p, parse, redact)
+	fs.VarPF(v, flagName, "", "")
+
+	actualValue, err := fs.GetString(flagName)
+	require.NoError(t, err)
+	assert.Equal(t, expectedRedactedValue, actualValue)
+
+	actualFlag := fs.Lookup(flagName)
+	require.NotNil(t, actualFlag)
+	assert.Equal(t, expectedRedactedValue, actualFlag.Value.String())
+	rs := actualFlag.Value.(*flags.Value[string])
+	require.NotNil(t, rs)
+	assert.Equal(t, expectedDefaultValue, rs.Unredacted().String())
+
+	fs.Set(flagName, unredactedValue)
+	assert.Equal(t, expectedRedactedValue, actualFlag.Value.String())
+	require.NotNil(t, rs)
+	assert.Equal(t, fmt.Sprintf("%v::%v", unredactedValue, unredactedValue), rs.Unredacted().String())
+}
+
+func (suite *ValueTestSuite) TestValueType() {
+	testCases := []struct {
+		testDescription string
+		giveValue       pflag.Value
+		wantTypeName    string
+	}{
+		{
+			testDescription: "string",
+			giveValue:       &flags.Value[string]{},
+			wantTypeName:    "string",
+		},
+		{
+			testDescription: "int",
+			giveValue:       &flags.Value[int]{},
+			wantTypeName:    "int",
+		},
+		{
+			testDescription: "bool",
+			giveValue:       &flags.Value[bool]{},
+			wantTypeName:    "bool",
+		},
+		{
+			testDescription: "RedactedString",
+			giveValue:       &flags.Value[flags.RedactedString]{},
+			wantTypeName:    fmt.Sprint(reflect.TypeOf(*new(flags.RedactedString))),
+		},
+		{
+			testDescription: "CustomString",
+			giveValue:       &flags.Value[flags.CustomString]{},
+			wantTypeName:    fmt.Sprint(reflect.TypeOf(*new(flags.CustomString))),
+		},
+		{
+			testDescription: "StringSlice",
+			giveValue:       &flags.Value[flags.StringSlice]{},
+			wantTypeName:    fmt.Sprint(reflect.TypeOf(*new(flags.StringSlice))),
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.testDescription, func() {
+			t := suite.T()
+			actualTypeName := tc.giveValue.Type()
+			assert.Equal(t, tc.wantTypeName, actualTypeName)
+		})
+	}
+}
+
+func TestValueTestSuite(t *testing.T) {
+	suite.Run(t, new(ValueTestSuite))
 }
