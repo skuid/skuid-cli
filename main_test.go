744117c339f6dfbd340d515932ecbffb436eebeb
techfg <barry@techfg.com>
2024-09-16 14:37:17 -0700

    test: avoid non-zero exit code due to no tests warning
    Related #213

--- main_test.go
+++ main_test.go
@@ -6,8 +6,10 @@ import (
 	"testing"
 
 	"github.com/gookit/color"
+	"github.com/stretchr/testify/assert"
 )
 
+// Skuid Review Required - See https://github.com/skuid/skuid-cli/issues/213
 func TestMain(m *testing.M) {
 	// call flag.Parse() here if TestMain uses flags
 	rc := m.Run()
@@ -34,4 +36,14 @@ func TestMain(m *testing.M) {
 	}
 	os.Exit(rc)
 
+}
+
+// There are no tests in `main_test` so `go test` will exit with an exit code of 1 (assuming all other tests pass)
+// and a warning that there "no tests to run".  This entire file could be removed since there aren't any tests
+// currently but until input is received on https://github.com/skuid/skuid-cli/issues/213, leaving `TestMain` as-is.
+// Given that, there needs to be at least one test to avoid the non-zero exit code which is why TestAvoidNoTestsWarning
+// exists.
+// TODO: Remove this test and possibly entire file once https://github.com/skuid/skuid-cli/issues/213 is addressed
+func TestAvoidNoTestsWarning(t *testing.T) {
+	assert.True(t, true)
 }
